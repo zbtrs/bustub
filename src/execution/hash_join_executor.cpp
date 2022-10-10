cd98c7e6da0b39833040d85e0db6cc24eae9c67d
zbtrs <1339623011@qq.com>
2022-10-10 16:59:51 +0800

    解决hash_join_executor.cpp的头文件问题

--- src/execution/hash_join_executor.cpp
+++ src/execution/hash_join_executor.cpp
@@ -11,6 +11,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/hash_join_executor.h"
+#include "execution/expressions/abstract_expression.h"
+#include "execution/expressions/column_value_expression.h"
 
 namespace bustub {
 
@@ -22,7 +24,18 @@ HashJoinExecutor::HashJoinExecutor(ExecutorContext *exec_ctx, const HashJoinPlan
 void HashJoinExecutor::Init() {
   left_child_ ->Init();
   right_child_ ->Init();
-  //
+  // build hash table
+  Tuple left_tuple;
+  RID left_rid;
+  while (left_child_ ->Next(&left_tuple,&left_rid)) {
+    auto value = plan_ ->LeftJoinKeyExpression() ->Evaluate(&left_tuple,plan_ ->GetLeftPlan() ->OutputSchema());
+    HashJoinKey hash_join_key{value}; //可以直接这样初始化？
+    if (hash_table_.count(hash_join_key) > 0) {
+      hash_table_[hash_join_key].emplace_back(std::move(left_tuple));
+    } else {
+      hash_table_.insert({hash_join_key,{left_tuple}});
+    }
+  }
 }
 
 auto HashJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
